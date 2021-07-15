f947182c77dcc27b2d690230327779f7d3ae180b
Taranchuk <nfhfyxer@gmail.com>
2021-07-15 18:32:19 +0300

    update

--- 1.3/Source/RimFantasy/AuraManager.cs
+++ 1.3/Source/RimFantasy/AuraManager.cs
@@ -15,21 +15,37 @@ namespace RimFantasy
 {
     public class RimFantasyManager : GameComponent
     {
+        public HashSet<ThingComp> compsToTickNormal = new HashSet<ThingComp>();
         public HashSet<CompGlowerStuffable> compGlowerToTick = new HashSet<CompGlowerStuffable>();
         public static RimFantasyManager Instance;
         public RimFantasyManager(Game game)
         {
-            Instance = this;
+            Init();
         }
 
+        void Init()
+        {
+            Instance = this;
+            if (compGlowerToTick is null)
+            {
+                compGlowerToTick = new HashSet<CompGlowerStuffable>();
+            }
+            if (compsToTickNormal is null)
+            {
+                compsToTickNormal = new HashSet<ThingComp>();
+            }
+        }
         public override void GameComponentTick()
         {
             foreach (var comp in compGlowerToTick)
             {
                 comp.Tick();
             }
+            foreach (var comp in compsToTickNormal)
+            {
+                comp.CompTick();
+            }
         }
-
         public override void ExposeData()
         {
             base.ExposeData();
