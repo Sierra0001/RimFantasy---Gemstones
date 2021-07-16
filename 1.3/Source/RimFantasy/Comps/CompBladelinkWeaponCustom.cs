2de5b07dc9c74e73feffc88851c3604e466f16df
Taranchuk <nfhfyxer@gmail.com>
2021-07-16 13:08:50 +0300

    update

---
+++ 1.3/Source/RimFantasy/Comps/CompBladelinkWeaponCustom.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using System.Reflection;
+using Verse;
+using UnityEngine;
+using HarmonyLib;
+using Verse.AI.Group;
+
+namespace RimFantasy
+{
+	public class CompProperties_BladelinkWeaponCustom : CompProperties_Biocodable
+	{
+		public List<WeaponTraitDef> weaponTraitsPool;
+		public CompProperties_BladelinkWeaponCustom()
+		{
+			compClass = typeof(CompBladelinkWeaponCustom);
+		}
+	}
+	public class CompBladelinkWeaponCustom : CompBladelinkWeapon
+	{
+		public new CompProperties_BladelinkWeaponCustom Props => base.props as CompProperties_BladelinkWeaponCustom;
+		private static readonly IntRange TraitsRange = new IntRange(1, 2);
+		public override void PostPostMake()
+		{
+			InitializeTraitsCustom();
+		}
+		public void InitializeTraitsCustom()
+        {
+			List<WeaponTraitDef> traits = Traverse.Create(this).Field("traits").GetValue<List<WeaponTraitDef>>();
+            IEnumerable<WeaponTraitDef> allDefs = Props.weaponTraitsPool;
+			if (traits == null)
+			{
+				traits = new List<WeaponTraitDef>();
+			}
+			Rand.PushState(parent.HashOffset());
+			int randomInRange = TraitsRange.RandomInRange;
+			for (int i = 0; i < randomInRange; i++)
+			{
+				IEnumerable<WeaponTraitDef> source = allDefs.Where((WeaponTraitDef x) => CanAddTrait(x, traits));
+				if (source.Any())
+				{
+					traits.Add(source.RandomElementByWeight((WeaponTraitDef x) => x.commonality));
+				}
+			}
+			Rand.PopState();
+		}
+		private bool CanAddTrait(WeaponTraitDef trait, List<WeaponTraitDef> traits)
+		{
+			if (!traits.NullOrEmpty())
+			{
+				for (int i = 0; i < traits.Count; i++)
+				{
+					if (trait.Overlaps(traits[i]))
+					{
+						return false;
+					}
+				}
+			}
+			return true;
+		}
+	}
+}
